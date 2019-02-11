b90a95a0e0c600ade64d03e13ce42afbc6a2424c
rcbensley <richardbensley@gmail.com>
2019-02-11 10:22:36 +0000

    Show grants.

---
+++ scripts/show-grants.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+SQL_USER="SELECT CONCAT('\'', user, '\'@\'', host, '\'') FROM mysql.user GROUP BY user,host"
+
+for u in $(mysql -NBe "${SQL_USER}")
+do
+	echo "Grants for ${u} on ${HOSTNAME}:"
+	mysql -NBe "SHOW GRANTS FOR ${u}" | sed -e "s/$/;/g"
+	echo ""
+done
