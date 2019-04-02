94cbc518505fcdbe3431f828056d9f645f22fac7
rcbensley <richardbensley@gmail.com>
2019-04-03 00:11:32 +0100

    Better example.

--- kvdb/test.py
+++ kvdb/test.py
@@ -1,13 +1,24 @@
 import kvdb
+from pprint import pprint as pp
 
 db = kvdb.db()
 
 db.setup()
 
-db.set('dvd', {'a': 1})
+db.set('Do Androids Dream of Electric Sheep',
+       {'format': 'Paperback',
+        'pages': '210',
+        'author': 'Phillip K. Dick'})
+db.set('Blade Runner',
+       {'format': 'DVD',
+        'running_time': '117 minutes'})
+db.set('Blade Runner 2049',
+       {'format': 'Blu-Ray',
+        'running_time': '163 minutes',
+        'Director': 'Denis Villeneuve'})
 
-print(db.get('dvd'))
+pp(db.get('Blade Runner'))
 
-db.update('dvd', {'a': 2})
+db.update('Blade Runner', {'Director': 'Ridley Scott'})
 
-print(db.get())
+pp(db.get())
