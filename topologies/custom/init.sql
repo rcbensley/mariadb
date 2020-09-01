d7f1d68bb04bd449d08c6f40aadcd57e901aad2c
rcbensley <richardbensley@gmail.com>
2020-09-01 20:13:27 +0100

    Custom instance for ludicrious talk

---
+++ topologies/custom/init.sql
@@ -0,0 +1,26 @@
+DELIMITER $$
+
+DROP PROCEDURE IF EXISTS auth.check_user_access$$
+CREATE DEFINER=root@localhost PROCEDURE auth.check_user_access()
+
+LANGUAGE SQL
+READS SQL DATA
+SQL SECURITY DEFINER
+
+BEGIN
+DECLARE v_current_user VARCHAR(256);
+DECLARE v_allowed TINYINT(1);
+DECLARE v_process_id INT;
+SET v_allowed = 0;
+
+SELECT SUBSTRING_INDEX(USER(),'@',1) INTO v_current_user;
+SELECT status FROM auth.users WHERE username=v_current_user INTO v_allowed;
+SELECT CONNECTION_ID() INTO v_process_id;
+
+IF v_allowed = 0 THEN
+	SELECT "User is not allowed to connect";
+	KILL v_process_id;
+END IF;
+
+END $$
+DELIMITER ;
---
+++ topologies/custom/setup.sql
@@ -0,0 +1,25 @@
+DROP DATABASE IF EXISTS auth;
+CREATE DATABASE IF NOT EXISTS auth;
+USE auth;
+
+SET PASSWORD FOR root@localhost=password('dev');
+
+GRANT SELECT ON *.* to 'testuser'@'localhost' IDENTIFIED BY 'trouble';
+
+CREATE TABLE users (username varchar(64) primary key not null, 
+	status tinyint(1) not null default 0);
+
+INSERT INTO auth.users VALUES ('root', 1), ('vagrant', 1), ('testuser', 0);
+
+# UPDATE auth.users SET status=0 WHERE username='testuser';
+# UPDATE auth.users SET status=1 WHERE username='testuser';
+
+DROP DATABASE IF EXISTS dba;
+CREATE DATABASE IF NOT EXISTS dba;
+USE dba;
+
+CREATE TABLE cnf (section char(16) flag=1,
+	VARIABLE_NAME varchar(64) flag=2,
+	VARIABLE_VALUE varchar(2048))
+	engine=CONNECT table_type=INI file_name='/mnt/db/my.cnf'
+	option_list='Layout=Row';
